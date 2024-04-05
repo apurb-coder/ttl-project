006f9178506e139e636c30eb4f8a82027ff22fd5
apurb-coder <55659698+apurb-coder@users.noreply.github.com>
2024-04-05 23:02:40 +0530

    Add input and button components, and handle form submission in Home.tsx

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,6 +1,12 @@
-import React from 'react'
+import React,{useState} from 'react'
+import { useNavigate } from 'react-router-dom';
 
 const Home:React.FC = () => {
+  const navigate =  useNavigate()
+  const [textInput, setTextInput] = useState("")
+  const handleSubmit = () =>{
+    navigate("/result")
+  }
   return (
     <div className="flex justify-center items-center h-screen">
       <div className="flex flex-col justify-center items-center">
@@ -12,8 +18,12 @@ const Home:React.FC = () => {
           name="input"
           id="input"
           className="mt-10 w-[30rem] h-[3rem] py-2 px-5 outline-none rounded-lg "
+          onChange={(e)=>setTextInput(e.target.value)}
         />
-        <button className="mt-10 bg-gradient-to-r from-orange-500 to-orange-800 py-3 px-5 rounded-md">
+        <button
+          className="mt-10 bg-gradient-to-r from-orange-500 to-orange-800 py-3 px-5 rounded-md"
+          onClick={handleSubmit}
+        >
           Submit
         </button>
       </div>
