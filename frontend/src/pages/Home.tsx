90d86c58314700773dc81746a2e9feb87fed6e3e
apurb-coder <55659698+apurb-coder@users.noreply.github.com>
2024-04-05 22:51:43 +0530

    Update font and scrollbar styles in index.css, and add input and button components in Home.tsx

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -2,8 +2,23 @@ import React from 'react'
 
 const Home:React.FC = () => {
   return (
-    <div>Home</div>
-  )
+    <div className="flex justify-center items-center h-screen">
+      <div className="flex flex-col justify-center items-center">
+        <span className="text-center font-bold bg-gradient-to-r from-orange-500 to-orange-800 bg-clip-text text-transparent text-5xl">
+          TEXT
+        </span>
+        <input
+          type="text"
+          name="input"
+          id="input"
+          className="mt-10 w-[30rem] h-[3rem] py-2 px-5 outline-none rounded-lg "
+        />
+        <button className="mt-10 bg-gradient-to-r from-orange-500 to-orange-800 py-3 px-5 rounded-md">
+          Submit
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default Home
