82c80e5965735e42415f6e5e2855d028109a68f2
apurb-coder <55659698+apurb-coder@users.noreply.github.com>
2024-04-05 23:42:15 +0530

    Add react-icons package and implement send button in Home.tsx

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,5 +1,6 @@
 import React,{useState} from 'react'
 import { useNavigate } from 'react-router-dom';
+import { IoSend } from "react-icons/io5";
 
 const Home:React.FC = () => {
   const navigate =  useNavigate()
@@ -13,19 +14,32 @@ const Home:React.FC = () => {
         <span className="text-center font-bold bg-gradient-to-r from-orange-500 to-orange-800 bg-clip-text text-transparent text-4xl tracking-wide">
           Credit Card Fraud Detection System
         </span>
-        <input
-          type="text"
-          name="input"
-          id="input"
-          className="mt-10 w-[30rem] h-[3rem] py-2 px-5 outline-none rounded-lg "
-          onChange={(e) => setTextInput(e.target.value)}
-        />
-        <button
+        <div className="flex items-center w-[30rem] h-[3rem] mt-20 relative">
+          <input
+            type="text"
+            name="input"
+            id="input"
+            className="w-full py-4 px-5 outline-none rounded-lg"
+            onChange={(e) => setTextInput(e.target.value)}
+            onKeyDown={(e) => {
+              if (e.key === "Enter") {
+                handleSubmit();
+              }
+            }}
+          />
+          <div
+            className="bg-gradient-to-r from-orange-500 to-orange-800 p-3 rounded-lg absolute right-2"
+            onClick={handleSubmit}
+          >
+            <IoSend />
+          </div>
+        </div>
+        {/* <button
           className="mt-10 bg-gradient-to-r from-orange-500 to-orange-800 py-2 px-5 rounded-md"
           onClick={handleSubmit}
         >
           Submit
-        </button>
+        </button> */}
       </div>
     </div>
   );
