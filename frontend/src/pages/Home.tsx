efca8bea9f851e8ed3df75656b36188ffc010976
apurb-coder <55659698+apurb-coder@users.noreply.github.com>
2024-04-05 23:11:12 +0530

    Update font and styles in index.css, and add Credit Card Fraud Detection System title in Home.tsx

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -10,18 +10,18 @@ const Home:React.FC = () => {
   return (
     <div className="flex justify-center items-center h-screen">
       <div className="flex flex-col justify-center items-center">
-        <span className="text-center font-bold bg-gradient-to-r from-orange-500 to-orange-800 bg-clip-text text-transparent text-5xl">
-          TEXT
+        <span className="text-center font-bold bg-gradient-to-r from-orange-500 to-orange-800 bg-clip-text text-transparent text-4xl tracking-wide">
+          Credit Card Fraud Detection System
         </span>
         <input
           type="text"
           name="input"
           id="input"
           className="mt-10 w-[30rem] h-[3rem] py-2 px-5 outline-none rounded-lg "
-          onChange={(e)=>setTextInput(e.target.value)}
+          onChange={(e) => setTextInput(e.target.value)}
         />
         <button
-          className="mt-10 bg-gradient-to-r from-orange-500 to-orange-800 py-3 px-5 rounded-md"
+          className="mt-10 bg-gradient-to-r from-orange-500 to-orange-800 py-2 px-5 rounded-md"
           onClick={handleSubmit}
         >
           Submit
