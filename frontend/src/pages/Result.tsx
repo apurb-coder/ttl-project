71372e13ba9d53567d8898d5d8d6d52b135d2935
apurb-coder <55659698+apurb-coder@users.noreply.github.com>
2024-04-09 00:02:32 +0530

    Update Result page with new design and layout in Result.tsx

--- frontend/src/pages/Result.tsx
+++ frontend/src/pages/Result.tsx
@@ -2,8 +2,13 @@ import React from 'react'
 
 const Result:React.FC = () => {
   return (
-    <div>Result</div>
-  )
+    <div className="flex flex-col justify-center items-center h-screen w-full">
+      <h1 className="text-center font-bold bg-gradient-to-r from-orange-500 to-orange-800 bg-clip-text text-transparent text-5xl tracking-wide">
+        Result
+      </h1>
+      <div className="mt-10">Result</div>
+    </div>
+  );
 }
 
 export default Result
